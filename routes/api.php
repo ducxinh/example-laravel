bb6225c3622f53ad2e178cd12aa7c45ca03da6ae
ducxinh <ducxinh.dn.201@gmail.com>
2024-12-02 13:47:26 +0700

    004 Setup CRUD API for Product management

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -20,3 +21,9 @@ Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->get('me', [AuthController::class, 'getMe']);
+
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('products', ProductController::class);
+});
